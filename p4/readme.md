315f472648dda02626455b9a0fcf896d4eeea558
cassidy-brown <cmb195@case.edu>
2016-10-07 19:45:22 -0400

    p4

---
+++ p4/readme.md
@@ -0,0 +1,13 @@
+# 2-DOF Robot
+
+Cassidy Brown - cmb195
+ROS P4 submission
+10/7/16
+
+This package includes a joint controller adapted from Part_2/minimal_joint_controller and
+a sin_commander adapted from my first project.
+A launch file, joint_controller.launch, has been added to launch all files and the 2DOF-robot.
+
+Nodes:
+	joint_controller_p4
+	sin_commander_p4
---
+++ p4/src/sin_commander.cpp
@@ -0,0 +1,50 @@
+// Cassidy Brown cmb195
+// sin_commander node for project 4
+// Ppublishes to pos_cmd and pos_cmd2
+#include<ros/ros.h> 
+#include<std_msgs/Float64.h> 
+#include<math.h>
+
+//global variables for callback functions to populate for use in main program 
+std_msgs::Float64 g_sin_vel_cmd;
+
+
+int main(int argc, char **argv) {
+    ros::init(argc, argv, "sin_commander_p4"); //name this node 
+    // when this compiled code is run, ROS will recognize it as a node called "sin_commander" 
+    ros::NodeHandle nh; // node handle 
+    //publish a velocity command computed by this controller; 
+    ros::Publisher pos_publisher = nh.advertise<std_msgs::Float64>("pos_cmd", 1);
+    ros::Publisher pos_publisher2 = nh.advertise<std_msgs::Float64>("pos_cmd2", 1);
+    double Kv = 2.0; // velocity feedback gain 
+    double dt_controller = 0.10; // 10Hz controller sample rate
+    double sample_rate = 1.0 / dt_controller; // compute the corresponding update frequency 
+    ros::Rate naptime(sample_rate); // use to regulate loop rate 
+
+    std_msgs::Float64 sin_pos_cmd;
+    sin_pos_cmd.data = 0.0; // init velocity command to zero
+    double radians = 0.0; // initialize at zero radians
+
+    std_msgs::Float64 sin_pos_cmd2;
+    sin_pos_cmd2.data = 0.0; // init velocity command to zero
+    double radians2 = 0.0; // initialize at zero radians
+
+    // enter the main loop: get velocity state and velocity commands 
+    // compute command force to get system velocity to match velocity command 
+    // publish this force for use by the complementary simulator 
+    while (ros::ok()) {
+        radians = radians + dt_controller * 3.14159265 / 2; // increment radian value by a magnitude of pi
+							//radians will increase by Pi/2 every second
+        //determine velocity to command with amplitude and frequency 
+        sin_pos_cmd.data = sin(radians * 0.8);  
+        sin_pos_cmd2.data = sin(radians * 1.2);   
+
+        pos_publisher.publish(sin_pos_cmd);
+        pos_publisher2.publish(sin_pos_cmd2);
+        ROS_INFO("position command = %f", sin_pos_cmd.data);
+        ROS_INFO("position command 2 = %f", sin_pos_cmd2.data);
+        ros::spinOnce(); //allow data update from callback; 
+        naptime.sleep(); // wait for remainder of specified period; 
+    }
+    return 0; // should never get here, unless roscore dies 
+} 
